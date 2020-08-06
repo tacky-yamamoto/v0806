0d1ac608e9f869b3e2f88f8e059479f73df4fdef
A601-22 <tnk.dat.class@gmail.com>
2020-08-06 09:31:32 +0900

    ラベル移動

--- v0806/Form1.cs
+++ v0806/Form1.cs
@@ -12,9 +12,22 @@ namespace v0806
 {
     public partial class Form1 : Form
     {
+        int vx = -10;
+        int vy = -10;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Left += vx;
+            label1.Top += vy;
         }
     }
 }
